84d4749cb7a8fd44712da10902c9b985fcaa9287
Cristian Ariel Chavez <cristianarielchavez@outlook.com.ar>
2023-03-06 17:02:33 -0300

    se agregan skills

--- main.js
+++ main.js
@@ -32,11 +32,12 @@ function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if (distancia_skills >= 300) {
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("kkk").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
+        document.getElementById("logistica").classList.add("barra-progreso1");
+        document.getElementById("liderazgo").classList.add("barra-progreso2");
+        document.getElementById("mejoracontinua").classList.add("barra-progreso3");
+        document.getElementById("calidadoperativa").classList.add("barra-progreso4");
+        document.getElementById("bi").classList.add("barra-progreso5");
+        document.getElementById("desarrollowebfront").classList.add("barra-progreso6");
+        document.getElementById("ingles").classList.add("barra-progreso7");
     }
-
 }
