7a06cc8880f8def365397d73c052b6ecc8fcadd0
Cristian Chavez <cristianarielchavez@outlook.com.ar>
2024-07-04 16:43:07 -0300

    cambio de diseño

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
 function seleccionar(link) {
     var opciones = document.querySelectorAll('#links  a');
-    opciones[0].className = "";
+
     opciones[1].className = "";
     opciones[2].className = "";
     opciones[3].className = "";
